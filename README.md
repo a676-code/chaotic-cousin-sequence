bd02ed903990f950eb916e152c3569dd528296d2
a676-code <109701008+thisusernameisaparadox@users.noreply.github.com>
2023-03-29 00:12:52 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# chaotic-cousin-sequence
+This script generates the [chaotic cousin sequence](https://www.youtube.com/watch?v=j0o-pMIR8uk). 
+
+output: `[1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 9, 10, 10, 10, 11, 13, 15, 15, 14, 15]`
+
+# Plotting with Seaborn
+## Basic Scatterplots
+Scatterplot of 10 numbers:
+![10 numbers](10.png)
+Scatterplot of 100 numbers:
+![100 numbers](100.png)
+Scatterplot of 1000 numbers:
+![1000 numbers](1000.png)
+Scatterplot of 10000 numbers:
+![10000 numbers](10000.png)
+Scatterplot of 100,000 numbers:
+![100,000 numbers](100000.png)
---
+++ chaotic_cousin_sequence.py
@@ -0,0 +1,55 @@
+# chaotic_cousin_sequence.py
+# Andrew Lounsbury
+# 29/3/23
+# Purpose: Generate the chaotic cousin sequence; https://www.youtube.com/watch?v=j0o-pMIR8uk
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def generate_chaotic_cousin(n):
+    a = [1, 1, 1]
+    i = 3
+    while len(a) < n:
+        a.append(a[a[i - 1]] + a[i - a[i - 2] - 1])
+
+        i += 1
+    return a
+
+print(generate_chaotic_cousin(30))
+
+# Basic Scatter Plots
+n = 10
+sequence = generate_chaotic_cousin(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Number", data=df)
+plt.show()
+
+n = 100
+sequence = generate_chaotic_cousin(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Number", data=df)
+plt.show()
+
+n = 1000
+sequence = generate_chaotic_cousin(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Number", data=df)
+plt.show()
+
+n = 10000
+sequence = generate_chaotic_cousin(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Number", data=df)
+plt.show()
+
+n = 100000
+sequence = generate_chaotic_cousin(n)
+df = pd.DataFrame(sequence, columns=["Number"])
+df['index'] = [i for i in range(n)]
+sns.scatterplot(x="index", y="Number", data=df)
+plt.show()
